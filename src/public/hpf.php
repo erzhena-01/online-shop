a6f42d62525118c4935a375278b92782d8010cf2
Эржена Куриганова <erzhena.kuriganova@gmail.com>
2025-04-24 19:39:45 +0900

    Регистрация с функцией

--- src/public/hpf.php
+++ src/public/hpf.php
@@ -1,125 +1,88 @@
 <?php
-
 $name = $_POST["name"];
 $email = $_POST["email"];
-$password = $_POST["psw"];
-$passwordRepeat = $_POST["psw-repeat"];
+$psw = $_POST["psw"];
+$pswRepeat = $_POST["psw-repeat"];
 
-$errors = [];
 
-if (strlen($name) <= 2) {
-    $errors['name'] = "Данное имя не подходит" . "\n";
+function IsValidateForm(): array
+{
+    $errors = [];
+
+
+
+    if (isset($_POST["name"])) {
+        $name = $_POST["name"];
+
+        if (strlen($name) <= 2) {
+            $errors['name'] = "Имя должно быть больше 2 символов";
+        }
+    } else {
+        $errors["name"] = "Имя должно быть заполнено";
+    }
+
+    if (isset($_POST["email"])) {
+        $email = $_POST["email"];
+
+        if (strlen($email) <= 2) {
+            $errors['email'] = "email должен быть больше 2 символов" . "\n";
+        } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $errors ['email'] = "email некорректный" . "\n";
+        }
+    } else {
+        $errors["email"] = "Email должен быть заполнен";
+    }
+
+    if (isset($_POST["psw"])) {
+        $psw = $_POST["psw"];
+
+        if (strlen($psw) <= 2) {
+            $errors['psw'] = "Пароль должен быть больше 2 символов";
+        }
+    } else {
+        $errors["psw"] = "Пароль должен быть заполнен";
+    }
+
+    if (isset($_POST["psw-repeat"])) {
+        $pswRepeat = $_POST["psw-repeat"];
+
+        if ($psw !== $pswRepeat) {
+            $errors['psw-repeat'] = "Пароль не соответствует" . "\n";
+        }
+    } else {
+        $errors["psw-repeat"] = "Пароль должен быть заполнен";
+    }
+
+    return [$errors, ['name' => $name, 'email' => $email, 'psw' => $psw]];
 
 }
 
-if (strpos($email, '@') === false || strlen($email) <= 2) {
-    $errors['email'] =  "email не подходит" . "\n";
-}
+$errors = IsValidateForm();
 
-if ($password !== $passwordRepeat) {
-    $errors['psw-rep'] = "Пароль не соответствует" . "\n";
-} else {
-    $errors['psw'] = "Вход успешно выполнен!" ."\n";
-}
+
 
 if(empty($errors)) {
 
     $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
 
-    $pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
+    password_hush($psw, PASSWORD_DEFAULT);
 
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :psw)");
+    $stmt->execute([':name' => $name, ':email' => $email, ':psw' => $psw]);
+
+
+    $result = $pdo->prepare("SELECT * FROM users WHERE email = :email;");
+    $result->execute([':email' => $email]);
+    $data = $result->fetch();
+    print_r($data);
+
+}else{
 
     print_r($errors);
 
 }
+
+
+require_once './registration_form.php';
 ?>
 
-<form action="hpf.php">
-    <div class="container">
-        <h1>Register</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr>
-
-        <label for="name"><b>Name</b></label>
-        <?php echo $errors['name']; ?>
-        <input type="text" placeholder="Enter Name" name="name" id="name" required>
-
-        <label for="email"><b>Email</b></label>
-        <?php echo $errors['email']; ?>
-        <input type="text" placeholder="Enter Email" name="email" id="email" required>
-
-        <label for="psw"><b>Password</b></label>
-        <?php echo $errors['psw']; ?>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <?php echo $errors['psw-repeat']; ?>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-        <hr>
-
-        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-        <button type="submit" class="registerbtn">Register</button>
-    </div>
-
-    <div class="container signin">
-        <p>Already have an account? <a href="#">Sign in</a>.</p>
-    </div>
-</form>
-
-<style>
-    * {box-sizing: border-box}
-
-    /* Add padding to containers */
-    .container {
-    padding: 16px;
-    }
-
-    /* Full-width input fields */
-    input[type=text], input[type=password] {
-width: 100%;
-        padding: 15px;
-        margin: 5px 0 22px 0;
-        display: inline-block;
-        border: none;
-        background: #f1f1f1;
-    }
-
-    input[type=text]:focus, input[type=password]:focus {
-    background-color: #ddd;
-        outline: none;
-    }
-
-    /* Overwrite default styles of hr */
-    hr {
-    border: 1px solid #f1f1f1;
-        margin-bottom: 25px;
-    }
-
-    /* Set a style for the submit/register button */
-    .registerbtn {
-    background-color: #04AA6D;
-        color: white;
-        padding: 16px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-        opacity: 0.9;
-    }
-
-    .registerbtn:hover {
-    opacity:1;
-}
-
-    /* Add a blue text color to links */
-    a {
-    color: dodgerblue;
-}
-
-    /* Set a grey background color and center the text of the "sign in" section */
-    .signin {
-    background-color: #f1f1f1;
-        text-align: center;
-    }
-
-</style>
--- src/public/registration_form.php
+++ src/public/registration_form.php
@@ -1,19 +1,33 @@
-<form action="handle_registration_form.php>
+<form action="hpf.php" method = "post">
     <div class="container">
         <h1>Register</h1>
         <p>Please fill in this form to create an account.</p>
         <hr>
 
         <label for="name"><b>Name</b></label>
+        <?php if (isset($errors['name'])): ?>
+        <label style="color: red"><?php echo $errors['name']; ?></label>
+
+        <?php endif; ?>
+
         <input type="text" placeholder="Enter Name" name="name" id="name" required>
 
         <label for="email"><b>Email</b></label>
+        <?php if (isset($errors['email'])): ?>
+        <label style="color: red"><?php echo $errors['email']; ?></label>
+        <?php endif; ?>
         <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
         <label for="psw"><b>Password</b></label>
+        <?php if (isset($errors['psw'])): ?>
+        <label style="color: red"><?php echo $errors['psw']; ?></label>
+        <?php endif; ?>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
         <label for="psw-repeat"><b>Repeat Password</b></label>
+        <?php if (isset($errors['psw-repeat'])): ?>
+        <label style="color: red"><?php echo $errors['psw-repeat']; ?></label>
+        <?php endif; ?>
         <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
         <hr>
 
